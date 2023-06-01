bfcdb260d5d2ada504b3dc2228bc2f369dd9bacd
Hugh <ygz14835187@163.com>
2023-06-01 17:34:50 +0800

    remove test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { calcCidV0 } from './src/utils/calcHash';
-const buff = fs.readFileSync('test.txt');
-const main = async () => {
-  console.log(await calcCidV0(buff));
-};
-
-main();
